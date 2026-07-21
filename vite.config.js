5e8e84bfb60a0d537868fe05eb9bd33e04503e1e
imel0n <hejieming09@gmail.com>
2026-07-21 10:44:32 +0800

    chore: remove vue devtools

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
     VitePWA({
       registerType: 'autoUpdate',
       // Let the SW work in `npm run dev` so you can test install/offline locally.
